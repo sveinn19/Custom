f3336bcfd9c298a17985c75e49a1f54680b1b5ec
Magnús Árni Magnússon <magnusm18@ru.is>
2021-05-11 12:44:26 +0000

    hello world stuff, ignore

--- hello_world/src/Controller/HelloWorldController.php
+++ hello_world/src/Controller/HelloWorldController.php
@@ -1,6 +1,10 @@
 <?php
 namespace Drupal\hello_world\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\hello_world\HelloWorldSalutation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
 /**
  * Controller for the salutation message.
  */
@@ -11,9 +15,36 @@ class HelloWorldController extends ControllerBase {
    * @return array
    *   Our message.
    */
+  /**
+   * The salutation service
+   * 
+   *  @var \Drupal\hello_world\HelloWorldSalutation
+   */
+protected $salutation;
+/**
+ * HelloWorldController constructor.
+ *
+ * @param \Drupal\hello_world\HelloWorldSalutation $salutation
+ */
+
+
+  public function __construct(HelloWorldSalutation $salutation){
+    $this -> salutation = $salutation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public static function create(ContainerInterface $container){
+    return new static(
+      $container -> get('hello_world.salutation')
+    );
+  }
+
   public function helloWorld() {
     return [
-      '#markup' => $this->t('Hello World'),
+      '#markup' => $this-> salutation -> getSalutation(),
     ];
   }
 }
