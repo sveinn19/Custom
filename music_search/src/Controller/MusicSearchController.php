eae23d1745cc717f596062b538aeee9b58cb3609
Sveinn Ubuntu <sveinni19@ru.is>
2021-05-12 17:35:51 +0000

    cerveza

--- music_search/src/Controller/MusicSearchController.php
+++ music_search/src/Controller/MusicSearchController.php
@@ -2,6 +2,8 @@
 namespace Drupal\music_search\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Render\Markup;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\music_search\modules\Spotify_lookup\SpotifyLookupService;
 /**
  * Controller for the salutation message.
  */
@@ -12,6 +14,22 @@ class MusicSearchController extends ControllerBase {
    * @return array
    *   Our message.
    */
+
+  protected $spotify_service;
+
+
+  public function __construct($spotify_service) {  
+    $this->spotify_service = $spotify_service;
+    }
+
+    /** * {@inheritdoc} 
+     * */
+  public static function create(ContainerInterface $container) { 
+     return new static(    
+       $container->get('Spotify_lookup.lookup')  
+       );
+     }
+
   public function musicSearchPrintFunc() {
     // $form = \Drupal::formBuilder()->getForm('Drupal\music_search\Form\MusicSearchForm');
 
@@ -23,7 +41,8 @@ class MusicSearchController extends ControllerBase {
   //  $request->attributes->get('_raw_variables')->get('user')
 
     return [
-      '#markup' => $this->t('name= '). t($sstring). t(' type= ') . t($type),
+      // '#markup' => $this->t('name= '). t($sstring). t(' type= ') . t($type),
+      '#markup' => $this->spotify_service->_spotify_api_get_query($sstring),
     ];
 
   }
