d4b8afa89ac4880d2ab385304ab1df364c78ca9b
Sveinn Ubuntu <sveinni19@ru.is>
2021-05-13 16:02:55 +0000

    searces for correct string

--- music_search/src/Controller/MusicSearchController.php
+++ music_search/src/Controller/MusicSearchController.php
@@ -41,10 +41,11 @@ class MusicSearchController extends ControllerBase {
   //  $request->attributes->get('_raw_variables')->get('user')
 
     //$test->_spotify_api_get_query('ABBA');
+    $uri = 'https://api.spotify.com/v1/search?q=' . $sstring . '&' . 'type=album';
 
     return [
       // '#markup' => $this->t('name= '). t($sstring). t(' type= ') . t($type),
-      '#markup' => http_build_query($this->spotify_service->_spotify_api_get_query('https://api.spotify.com/v1/search?q=abba&type=album')), //_spotify_api_get_query($sstring),
+      '#markup' => http_build_query($this->spotify_service->_spotify_api_get_query($uri)), //_spotify_api_get_query($sstring),
     ];
 
     // return $this->spotify_service->_spotify_api_get_query('https://api.spotify.com/v1/search?q=abba&type=album');
