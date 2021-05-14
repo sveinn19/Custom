72a4aee0a2a387581fea09c2a78d28d723118911
Magnús Árni Magnússon <magnusm18@ru.is>
2021-05-14 16:21:49 +0000

    Added content creation from spotify json, need more implementation though but connection works.

--- music_search/src/Form/CreateContentForm.php
+++ music_search/src/Form/CreateContentForm.php
@@ -19,11 +19,11 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
     public function buildForm(array $form, FormStateInterface $form_state){
         $result = $_SESSION['s1'][$_SESSION['s2'] . 's']['items'][$_SESSION['con']];
-
+        $_SESSION['result'] = $result;
 
         $form['text'] = array(
             '#type' => 'textfield',
-            '#title' => $result['followers']['total'],
+            '#title' => 'Titillinn',
             '#suffix' => "<pre>".print_r($result, true)."</pre>",
         );
 
@@ -38,7 +38,23 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
     public function submitForm(array &$form, FormStateInterface $form_state){
         \Drupal::messenger()->addMessage(t('CreateContent'));
-        
+
+        $result = $_SESSION['result'];
+        $url = substr($result['external_urls']['spotify'], 8, strlen($result['external_urls']['spotify']));
+        $img_path = ""
+
+        $node = Node::create([
+            'type'  => 'listamadur',
+            'title' =>  $result['name'],
+          ]);
+          $node->field_nafn->value = $result['name'];
+          $node->set('field_vefsida_listamanns', [
+            'uri' => $result['external_urls']['spotify'],
+            'title' => $result['external_urls']['spotify'],
+            'options' => [],
+          ]);
+
+          $node->save();
     }
 
  }
